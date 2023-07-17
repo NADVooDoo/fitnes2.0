4e7fdc5c7e143f867d33b70c554e365605d1a347
Nikolai Didur <nadvoodoo@gmail.com>
2023-07-17 21:47:25 +0300

    Добавляет блок Тренажерный зал

---
+++ source/js/utils/load-script.js
@@ -0,0 +1,14 @@
+const loadScript = (src, handleLoad, callback) => {
+  const scriptElement = document.createElement('script');
+  scriptElement.src = src;
+
+  scriptElement.addEventListener('load', handleLoad);
+
+  if (callback) {
+    callback(scriptElement);
+  }
+
+  document.body.append(scriptElement);
+};
+
+export {loadScript};
