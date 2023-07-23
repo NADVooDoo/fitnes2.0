3b7c040c66b2aa7c2a9b11528ed509f02027a4ee
Nikolai Didur <nadvoodoo@gmail.com>
2023-07-23 12:24:31 +0300

    Добавляет блоки  SUPERGAMES, Тренеры, Акции

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
 import {initVideos} from './modules/video';
 import {initTabs} from './modules/init-tabs';
+import {getSlider} from './modules/init-swipers';
 
 // ---------------------------------
 
@@ -18,6 +19,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    getSlider();
     const form = new Form();
     window.form = form;
     form.init();
